f75a7bc9c599895172bc13c0ce5399a0b0a5737c
Seshagiri Sriram <SeshagiriSriram@gmail.com>
2025-04-23 13:23:00 +0000

    Have to find a way to avoid dos/unix clashes

--- kubernetes/startminikube.sh
+++ kubernetes/startminikube.sh
@@ -2,7 +2,6 @@
 #docker context use rootless 
 # Adapt the below as necessary
 minikube start --vm-driver=docker --container-runtime=containerd --memory 6144 --cpus 4   
-
 #minikube addons enable metrics-server
 #minikube addons enable default-storageclass
 #minikube addons enable storage-provisioner
