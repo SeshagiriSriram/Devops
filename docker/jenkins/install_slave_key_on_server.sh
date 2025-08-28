680fc3728a44fcadda83e3cec82d8cf7ba728e1b
Seshagiri Sriram <seshagirisriram@gmail.com>
2025-08-28 11:09:50 +0530

    Buggy and to DO : insall keys from master/slaves

--- docker/jenkins/install_slave_key_on_server.sh
+++ docker/jenkins/install_slave_key_on_server.sh
@@ -1,5 +1,5 @@
 cd /var/jenkins_home/.ssh
 scp ubuntu@jenkins_slave:/home/ubuntu/.ssh/id_rsa.pub  id_remote
-cat /home/ubuntu/.ssh/id_remote authorized_keys
+cat id_remote >> authorized_keys
 chmod 600 authorized_keys
 rm id_remote
